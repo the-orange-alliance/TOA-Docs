6e4f0d166824de85f8a0bf5c6b853bedcb58bd87
Josh Bacon <bacon.josh09@gmail.com>
2021-11-22 17:26:21 -0500

    Autogeneration of _index.yml

---
+++ autogen_index.py
@@ -0,0 +1,18 @@
+import glob
+from os.path import basename, relpath
+
+# AUTOGENERATES THE _INDEX.YML FILE IN ./OPENAPI/TYPES
+
+files = glob.glob("./openapi/types/**/[!_]*.yml", recursive=True)
+
+output = ""
+
+for file in files:
+    name = basename(file)
+    relative_path = relpath(file, "./openapi/types")
+    output += f'{name}:\n  $ref: "./{relative_path}"\n'
+
+with open("./openapi/types/_index.yml", "w") as f:
+    f.write(output)
+
+
