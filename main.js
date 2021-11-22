c8bc42534b37cf39e22c7aa170af9eb7c8b8db11
Josh Bacon <bacon.josh09@gmail.com>
2021-11-22 14:43:56 -0500

    Change base pathname of openapi definition file

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ import "./style.css";
 import SwaggerUI from "swagger-ui";
 import "swagger-ui/dist/swagger-ui.css";
 
+const path = location.pathname;
+
 SwaggerUI({
     dom_id: "#app",
-    url: "/openapi.yml",
+    url: `${pathname}openapi.yml`,
 });
